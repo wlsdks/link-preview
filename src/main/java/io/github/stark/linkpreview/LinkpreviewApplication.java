7afa5b1b99e87ba77f7ddf53f2302a266245d512
wlsdks <wlsdks12@naver.com>
2025-01-05 22:30:40 +0900

    :sparkles: 초기 프로젝트 세팅

---
+++ src/main/java/io/github/stark/linkpreview/LinkpreviewApplication.java
@@ -0,0 +1,13 @@
+package io.github.stark.linkpreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkpreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkpreviewApplication.class, args);
+	}
+
+}
